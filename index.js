4187444179759b76f08a64c0f26ceab5a38a9425
pearlyadav <yadavpearl2001@gmail.com>
2020-02-02 03:34:29 +0530

    JavaScript that will let you view your ratings

---
+++ index.js
@@ -0,0 +1,71 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  
+  if(typeof process.argv[2] !== 'undefined' && process.argv[2]){
+
+    await page.goto(`https://www.codechef.com/users/${process.argv[2]}`, {waitUntil: 'domcontentloaded'});
+
+    const CodeChefRating = await page.evaluate( () => {
+      let ratingList = document.querySelectorAll(".rating-number");
+      const b = [...ratingList];
+      if(typeof b[0]!=='undefined' && b[0]){
+        return `CodeChef Rating: ${b[0].innerText}`;
+      }
+      else{
+        return "Sorry, Cannot Get CodeChef Rating at this moment. Please try again later!!!";
+        }
+    });
+    console.log(CodeChefRating); 
+    // }else{
+    //     console.log("Error! Cannot connect to Internet");        
+    // }
+}
+else{
+    console.log("Provide your CodeChef Handle!!!");
+}
+
+//   Hackerearth
+if(typeof process.argv[3] !== 'undefined' && process.argv[3]){
+  const page2 = await browser.newPage();
+  await page2.goto(`https://www.hackerearth.com/@${process.argv[3]}`, {waitUntil: 'domcontentloaded'});
+  
+  const HackerearthRating = await page2.evaluate( () => {
+    let ratingList = document.querySelectorAll(".large.darker.float-right.weight-600");
+    const b = [...ratingList];
+    if(typeof b[2]!=='undefined' && b[2]){
+        return `HackerEarth Rating: ${b[2].innerText}`;
+      }
+      else{
+        return "Sorry, Cannot Get HackerEarth Rating at this moment. Please try again later!!!";
+        }
+  });
+  console.log(HackerearthRating);
+}
+else{
+    console.log("Provide your HackerEarth Handle!!!");
+}
+//   CodeForces
+if(typeof process.argv[4] !== 'undefined' && process.argv[4]){
+const page3 = await browser.newPage();
+await page3.goto(`https://codeforces.com/profile/${process.argv[4]}`, {waitUntil: 'domcontentloaded'});
+
+const CodeforcesRating = await page3.evaluate( () => {
+  let ratingList = document.querySelectorAll(".user-gray");
+  const b = [...ratingList];
+  if(typeof b[4]!=='undefined' && b[4]){
+    return `Codeforces Rating: ${b[4].innerText}`;
+  }
+  else{
+    return "Sorry, Cannot Get Codeforces Rating at this moment. Please try again later!!!";
+    }
+});
+console.log(CodeforcesRating);
+}
+else{
+    console.log("Provide your Codeforces Handle!!!");
+} 
+  await browser.close();
+})();
